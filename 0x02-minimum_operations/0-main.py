556e08991e4d94b24dec32329b8b6446540f4544
isahjohnitopa <isahjohna@gmail.com>
2024-10-17 16:08:52 +0200

    Minimum Operations

---
+++ 0x02-minimum_operations/0-main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Main file for testing
+"""
+
+
+minOperations = __import__('0-minoperations').minOperations
+
+n = 12
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Contains a method that calculates the fewest number of operations
+needed to result in exactly n H characters in th files.
+"""
+
+
+def minOperations(n):
+    """
+    Function that calculate the needed n H characters in file
+    Args:
+        n: The number of H
+    Returns:
+        The minimum operation
+    """
+    operations = 0
+    min_operations = 2
+    while n > 1:
+        while n % min_operations == 0:
+            operations += min_operations
+            n /= min_operations
+        min_operations += 1
+    return operations
